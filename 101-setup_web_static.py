84f6daa461eea6ab3f347872fa7932ce613ae815
DRWShub <ericgodworks@yahoo.com>
2023-07-23 20:28:06 -0700

    setup web static

--- 101-setup_web_static.py
+++ 101-setup_web_static.py
@@ -1,18 +1,21 @@
-# Puppet for setup
-
+#Puppet file to configure nginx web server
+#Configuration file
 $nginx_conf = "server {
     listen 80 default_server;
     listen [::]:80 default_server;
     add_header X-Served-By ${hostname};
-    root   /var/www/html;
-    index  index.html index.htm;
+    root /var/www/html;
+    index index.html index.htm;
+
+    location /redirect_me {
+        return 301 https://youtube.com;
+    }
+
     location /hbnb_static {
         alias /data/web_static/current;
         index index.html index.htm;
     }
-    location /redirect_me {
-        return 301 http://linktr.ee/firdaus_h_salim/;
-    }
+   
     error_page 404 /404.html;
     location /404 {
       root /var/www/html;
@@ -23,65 +26,76 @@ $nginx_conf = "server {
 package { 'nginx':
   ensure   => 'present',
   provider => 'apt'
-}
+} ->
 
--> file { '/data':
+file { '/data/':
+  ensure  => directory,
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  recurse => true,
+} ->
+
+file { '/data/web_static':
   ensure  => 'directory'
-}
+} ->
 
--> file { '/data/web_static':
-  ensure => 'directory'
-}
+file { '/data/web_static/releases':
+  ensure  => 'directory'
+} ->
 
--> file { '/data/web_static/releases':
-  ensure => 'directory'
-}
+file { '/data/web_static/releases/test':
+  ensure  => 'directory'
+} ->
 
--> file { '/data/web_static/releases/test':
-  ensure => 'directory'
-}
+file { '/data/web_static/shared':
+  ensure  => 'directory'
+} ->
 
--> file { '/data/web_static/shared':
-  ensure => 'directory'
-}
-
--> file { '/data/web_static/releases/test/index.html':
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "this webpage is found in data/web_static/releases/test/index.htm \n"
-}
+  content => '<html>
+                 <head>
+                 </head>
+                 <body>
+                   Holberton School
+                 </body>
+               </html>',
+} ->
 
--> file { '/data/web_static/current':
-  ensure => 'link',
-  target => '/data/web_static/releases/test'
-}
-
--> exec { 'chown -R ubuntu:ubuntu /data/':
-  path => '/usr/bin/:/usr/local/bin/:/bin/'
-}
+file { '/data/web_static/current':
+  ensure  => 'link',
+  target  => '/data/web_static/releases/test'
+} ->
 
 file { '/var/www':
-  ensure => 'directory'
-}
+  ensure  => 'directory'
+} ->
 
--> file { '/var/www/html':
-  ensure => 'directory'
-}
+file { '/var/www/html':
+  ensure  => 'directory'
+} ->
 
--> file { '/var/www/html/index.html':
+file { '/var/www/html/index.html':
   ensure  => 'present',
-  content => "This is my first upload  in /var/www/index.html***\n"
-}
+  content => '<html>
+                 <head>
+                 </head>
+                 <body>
+                   Holberton School
+                </body>
+               </html>',
+} ->
 
--> file { '/var/www/html/404.html':
+file { '/var/www/html/404.html':
   ensure  => 'present',
-  content => "Ceci n'est pas une page - Error page\n"
-}
+  content => "Ceci n'est pas une page"
+} ->
 
--> file { '/etc/nginx/sites-available/default':
+file { '/etc/nginx/sites-available/default':
   ensure  => 'present',
   content => $nginx_conf
-}
+} ->
 
--> exec { 'nginx restart':
+exec { 'nginx restart':
   path => '/etc/init.d/'
 }
