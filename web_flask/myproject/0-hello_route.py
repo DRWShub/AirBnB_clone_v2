ce1a6709461101185d4446b865da97ca3d1c23e6
DRWShub <ericgodworks@yahoo.com>
2023-07-23 22:01:32 -0700

    0

--- web_flask/myproject/0-hello_route.py
+++ web_flask/myproject/0-hello_route.py
@@ -5,13 +5,13 @@ Your web application must be listening on 0.0.0.0, port 5000
 
 from flask import Flask
 
-app = Flask("__name__")
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
     """Return a given string"""
-    return ("Hello HBNB!")
+    return '<h1>Hello HBNB!</h1>'
 
 
 if __name__ == "__main__":
