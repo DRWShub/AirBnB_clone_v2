268bf2db1e1d7a2312aa3fac085ce2fb37c21092
DRWShub <ericgodworks@yahoo.com>
2023-07-23 19:59:54 -0700

    clean web static

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
+"""Script to delete outdated version from archives"""
 import os
 from fabric.api import *
 
-env.hosts = ['100.25.19.204', '54.157.159.85']
+env.user = "ubuntu"
+env.hosts = ["100.25.118.189", "34.234.204.223"]
+env.key_filename = ["~/.ssh/id_rsa", "~/.ssh/id_rsa_new"]
 
 
 def do_clean(number=0):
-    """Delete out-of-date archives.
+    """function to delete outdated versions"""
+    for host, key_file in zip(env.hosts, env.key_filename):
+        env.host_string = host
+        env.key_filename = key_file
 
-    Args:
-        number (int): The number of archives to keep.
-
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
     number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
+    archvs = sorted(os.listdir("versions"))
+    [archvs.pop() for i in range(number)]
     with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+        [local("rm ./{}".format(a)) for a in archvs]
 
     with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+        archvs = run("ls -tr").split()
+        archvs = [a for a in archvs if "web_static_" in a]
+        [archvs.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archvs]
