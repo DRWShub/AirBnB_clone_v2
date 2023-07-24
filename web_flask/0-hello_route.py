caf5fba0a97034af5203a0712a4f05d5096c6af1
DRWShub Name <ericgodworks@yahoo.com>
2023-07-24 08:23:17 -0700

    testing 0 route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,21 +3,6 @@
 Your web application must be listening on 0.0.0.0, port 5000
 """
 
-from flask import Flask
-
-app = Flask("__name__")
-
-
-@app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
-
-
 from flask import Flask, request
 from flask_restful import Resource, Api
 import sys
@@ -25,15 +10,17 @@ import os
 
 app = Flask(__name__)
 api = Api(app)
-port = 5000
+port = 5100
 
 if sys.argv.__len__() > 1:
     port = sys.argv[1]
 print("Api running on port : {} ".format(port))
 
+
 class topic_tags(Resource):
     def get(self):
-        return {'Hello HBNB!'}
+        return {'hello': 'world world'}
+
 
 api.add_resource(topic_tags, '/')
 
